21d0af9e8d63ff596f684595003e2a33378c1e02
Volodymyr <volodymyrquo@gmail.com>
2020-11-21 12:03:31 +0200

    added app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import '/App.css';
+import './App.css';
 
 export default class App extends Component {
   state = { advice: '' };
