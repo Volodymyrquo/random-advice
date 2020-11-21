d5437c5d5323d611cac0fea2da4a5584076cf59f
Volodymyr <volodymyrquo@gmail.com>
2020-11-21 11:54:43 +0200

    created axios query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import '/App.css';
 
 export default class App extends Component {
+  state = { advice: '' };
+  componentDidMount() {
+    this.fetchedAdvice();
+  }
+
+  fetchedAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => console.log(error));
+  };
   render() {
+    const { advice } = this.state;
     return (
-      <div>
-        <h1>App</h1>
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+        </div>
       </div>
     );
   }
